dc10ec5d4de94a46afc8f988c587ef8c754058dc
unknown <ahmedalariqi97@gmail.com>
2024-11-10 12:55:10 +0300

    add footer code

---
+++ resources/views/partials/front/footer.blade.php
@@ -0,0 +1,42 @@
+<!-- start footer section -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 img">
+                <img src="images/footerLogo.png" class="" alt="" />
+
+            </div>
+            <div class="col-12 social-icons">
+                <div class="flex">
+                    <a href="https://www.instagram.com/accounts/login"><i class='bx bxl-instagram-alt' ></i></a>
+                    <a href="https://www.facebook.com"><i class='bx bxl-facebook'></i></a>
+                    <a href="https://twitter.com/i/flow/login"><i class='bx bxl-twitter' ></i></a>
+                   
+                    <a href="https://github.com/login"><i class='bx bxl-github'></i></a>
+                    <a href="https://www.youtube.com/login"><i class='bx bxl-youtube'></i></a>
+                 </div>
+
+
+            </div>
+            <br />
+
+            <div class="col-12 address-links">
+                <a href="#" class="">
+                    <i class='bx bx-phone phone-icon' ></i>
+                    <span>920003090</span>
+                </a>
+                <a href="#" class="">
+                    <i class='bx bx-mail-send' ></i>
+                    <span>info@alsalam-taxi.com</span>
+                </a>
+                <a href="#" class="">
+                    <i class='bx bx-location-plus' ></i>
+                    <span>المملكة العربية السعودية - المدينة المنورة - قربان</span>
+                </a> 
+            </div>
+        </div>
+    </div>
+
+ </footer>
+
+<!-- end footer section -->
