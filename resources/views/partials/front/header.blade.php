bba57de7ee3a38d64fe8c926761fc7c20b28139c
unknown <ahmedalariqi97@gmail.com>
2024-11-10 12:54:24 +0300

    add header code

---
+++ resources/views/partials/front/header.blade.php
@@ -0,0 +1,36 @@
+<!-- Header -->
+<header class="header">
+            <nav class="nav container flex">
+                    <a href="#" class="logo-content flex">
+                        <img src="images/headerLogo.png" class="logo-img" alt="Taxi" />
+                    </a>
+
+                    <div class="menu-content">
+                            <ul class="menu-list flex">
+                                    <li><a href="#" class="nav-link">الخصوصية وسياسة الاستخدام</a></li>
+                                    <li><a href="#" class="nav-link">الأحكام والشروط</a></li>
+                                    <li><a href="#" class="nav-link">سياسة العملاء</a></li>
+                                    <li><a href="#" class="nav-link">سياسة السائقين</a></li>
+                                    <li><a href="#" class="nav-link">English</a></li>
+                            </ul>
+
+                            <div class="media-icons flex">
+                                    <a href="https://www.facebook.com"><i class='bx bxl-facebook'></i></a>
+                                    <a href="https://twitter.com/i/flow/login"><i class='bx bxl-twitter' ></i></a>
+                                    <a href="https://www.instagram.com/accounts/login"><i class='bx bxl-instagram-alt' ></i></a>
+                                    <a href="https://www.youtube.com/login"><i class='bx bxl-youtube'></i></a>
+                            </div>
+
+                            <i class='bx bx-x navClose-btn'></i>
+                        </div>
+                        
+                        <div class="contact-content flex">
+                            <i class='bx bx-phone phone-icon' ></i>
+                            <span class="phone-number">تواصل معنا</span>
+                        </div>
+
+                        <i class='bx bx-menu navOpen-btn'></i>
+                        <!-- <i class="fa-solid fa-bars navOpen-btn"></i> -->
+                </nav>
+        
+    </header>
